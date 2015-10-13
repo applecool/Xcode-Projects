9a69c13056d0ea29c05a1a1e0bedd8ee3994afcc
applecool <kmitharsha@gmail.com>
2015-10-13 10:10:12 -0500

    Added Tip Calculator

---
+++ Tip Calculator/Tip Calculator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by ShellZero on 10/13/15.
+//  Copyright © 2015 ShellZero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtMealCost: UITextField!
+    @IBOutlet weak var txtTipPercentage: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
