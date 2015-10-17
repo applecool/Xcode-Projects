35573b4b2c286fb1c0c705f93f082afeef061316
applecool <kmitharsha@gmail.com>
2015-10-17 12:00:29 -0400

    added planets viewer

---
+++ ImageViewer/ImageViewer WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,50 @@
+//
+//  InterfaceController.swift
+//  ImageViewer WatchKit Extension
+//
+//  Created by ShellZero on 10/17/15.
+//  Copyright © 2015 ShellZero. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet var sldSlider: WKInterfaceSlider!
+    @IBOutlet var imgImage: WKInterfaceImage!
+    
+    var numberOfImage : Float = 0
+    var imageFileTypeJPG : String = "jpg"
+    
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        displayImage()
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    @IBAction func sldImageSliderAction(value: Float) {
+        numberOfImage = value
+        displayImage()
+    }
+    
+    func displayImage(){
+        imgImage.setImage(UIImage(named: "\(numberOfImage)" + imageFileTypeJPG))
+    }
+    
+    
+    
+}
