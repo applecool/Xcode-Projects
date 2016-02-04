f0b0acbecfaf0caad8a89c078e2655e82eba426a
applecool <kmitharsha@gmail.com>
2016-02-03 22:53:22 -0600

    added ContactViewController

---
+++ VintageStore/VintageStore/ContactViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ContactViewController.swift
+//  VintageStore
+//
+//  Created by ShellZero on 2/3/16.
+//  Copyright © 2016 ShellZero. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.addSubview(scrollView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.contentSize = CGSizeMake(375, 800)
+    }
+
+
+}
